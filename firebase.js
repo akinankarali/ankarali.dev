a6eccc3bd699be0d07d448c11884117800f63efb
Saim Akın Ankaralı <saim.ankarali@kariyer.net>
2024-11-03 23:25:51 +0300

    feat: add firebase analytics

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -17,4 +18,9 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-export { db, storage };
+let analytics;
+if (typeof window !== "undefined" && firebaseConfig.measurementId) {
+  analytics = getAnalytics(app);
+}
+
+export { db, storage, analytics };
